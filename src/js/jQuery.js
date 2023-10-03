c1d5383460f064b3fb65523502b94d38eb9308dd
ChandeepSharma <chandeep.sharma@lpinfotech.com>
2023-10-03 10:52:46 +0530

    add aside bar

--- src/js/jQuery.js
+++ src/js/jQuery.js
@@ -9,4 +9,16 @@ $(document).ready(function(){
         $("#closeBtn").hide();
         $("nav .col-span-12").toggle("2000");
     })
+    $("#asideBarBtn").click(function(){
+        $("aside").hide("500");
+        $("main").addClass("mx-auto")
+        $("#asideBarBtnMain").show("500")
+    })
+    $("#asideBarBtnMain").click(function(){
+        $("aside").show("500");
+        // $("main").removeClass("mx-auto")
+        $(this).hide("500");
+
+
+    })
 })
